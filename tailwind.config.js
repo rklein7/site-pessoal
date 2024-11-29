01060a8e5845afa7f507961bba853a20eaf4740d
Rafael Klein <rafael.augustoklein04@gmail.com>
2024-11-28 21:38:43 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Certifique-se de que os arquivos React são considerados
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
