d06674f39357bcc0dc50e09b96ac17d7fedf2251
Rafael Klein <rafael.augustoklein04@gmail.com>
2024-12-09 00:23:34 -0300

    alterando sobre, inserindo certificados e alterando contato

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { ArrowRight, Code, Github, Instagram, Linkedin } from 'lucide-react';
-// import FotoPessoal from './assets/fotoPessoal.png';
+import { ArrowRight, Code, Github, Instagram, Linkedin, Mail } from 'lucide-react';
+import FotoPessoal from './assets/fotoPessoal.png';
 import onCampus from './assets/OnCampus.png'
 import planner from './assets/Planner.png'
 import starWars from './assets/starWarsHome.png'
 import dolarEuro from './assets/dolarEuro.png'
 
 const App: React.FC = () => {
+  const certificates = [
+    {
+      title: "Arquiteto de Soluções em Cloud - 3° semestre",
+      issuer: "Atitus Educação",
+      date: "Julho de 2024",
+      skills: ["Cibersegurança", "Amazon Web Services"],
+      path: "https://www.linkedin.com/in/rafael-augusto-klein-0a9770268/details/certifications/1730077276732/single-media-viewer/?profileId=ACoAAEGby4EByy3MJoXe15tsV6aSB9dmPLJF1B8"
+    },
+    {
+      title: "NLW Journey - ReactJS",
+      issuer: "Rocketseat",
+      date: "Julho de 2024",
+      skills: [
+        "React"
+      ],
+      path: "https://app.rocketseat.com.br/certificates/d13ff0d7-f229-4af8-8524-f47fc499b2de"
+    },
+    {
+      title:
+        "AWS Academy Graduate - AWS Academy Introduction to Cloud Semester 1",
+      issuer: "Amazon Web Services",
+      date: "Junho de 2024",
+      skills: [
+        "Amazon Web Services"
+      ],
+      path: "https://www.credly.com/badges/9c4794c9-6a66-454a-b84b-d423d85d46f1/linked_in_profile"
+    },
+    {
+      title: "Desenvolvimento de soluções - 2° semestre",
+      issuer: "Atitus Educação",
+      date: "Junho de 2024",
+      skills: [
+        "Desenvolvimento Java Backend e SQL",
+        "Desenvolvimento HTML, CSS e Javascript Frontend",
+      ],
+      path: "https://www.even3.com.br/documentos/imprimir?i=97425931.5683398.3.7.8921764153667368&cc=257F2ACD-A3DC-427D-AC56-26B7F7F7C396"
+    },
+    {
+      title: "NLW Pocket: Javascript - Full-stack",
+      issuer: "Rocketseat",
+      date: "Setembro de 2024",
+      skills: [
+        "Node.JS",
+        "React",
+      ],
+      path: "https://app.rocketseat.com.br/certificates/35655d56-2ab3-4cf2-9dab-8a3baf4098d4"
+    },
+  ];
+
   return (
     <div className="bg-black text-gray-300 font-sans min-h-screen">
       <header className="absolute top-0 left-0 right-0 z-10">
@@ -44,14 +93,14 @@ const App: React.FC = () => {
         <div className="container mx-auto px-6">
           <h3 className="text-4xl font-bold text-center mb-6 text-zinc-100">Sobre</h3>
           <hr className="w-72 border-t border-zinc-400 mx-auto mt-2 mb-10" />
-          <div className="flex flex-col md:flex-row justify-between items-start space-y-6 md:space-y-0">
-            <div className="md:w-1/2 text-start md:text-left">
-              <p className="text-xl text-zinc-300 mb-4">
-                Olá! Sou o Rafael, estudante de Ciência da Computação na Atitus Educação - Passo Fundo. Desenvolvedor FullStack apaixonado por tecnologia. Trabalho com diversas ferramentas e linguagens de programação, buscando sempre entregar soluções eficientes e de alta qualidade. Meu objetivo é continuar aprendendo e aprimorando minhas habilidades para contribuir no desenvolvimento de sistemas
-                inovadores e impactantes.
+          <div className="flex flex-col md:flex-row items-center md:items-center gap-x-10">
+            {/* Texto */}
+            <div className="md:w-1/2 text-start">
+              <p className="text-xl text-zinc-300 mb-4 leading-relaxed">
+                Olá! Sou o Rafael, estudante de Ciência da Computação na Atitus Educação - Passo Fundo. Desenvolvedor FullStack apaixonado por tecnologia. Trabalho com diversas ferramentas e linguagens de programação, buscando sempre entregar soluções eficientes e de alta qualidade. Meu objetivo é continuar aprendendo e aprimorando minhas habilidades para contribuir no desenvolvimento de sistemas inovadores e impactantes.
               </p>
 
-              <ul className="mt-8 space-y-4 text-left">
+              <ul className="mt-8 space-y-6">
                 <li>
                   <a
                     href="https://www.linkedin.com"
@@ -60,9 +109,8 @@ const App: React.FC = () => {
                     <Linkedin className="w-6 h-6" />
                     LinkedIn
                   </a>
-                  <hr className="w-72 border-t border-zinc-400 mt-2" />
+                  <hr className="w-full border-t border-zinc-400 mt-2" />
                 </li>
-
                 <li>
                   <a
                     href="https://github.com"
@@ -71,9 +119,8 @@ const App: React.FC = () => {
                     <Github className="w-6 h-6" />
                     GitHub
                   </a>
-                  <hr className="w-72 border-t border-zinc-400 mt-2" />
+                  <hr className="w-full border-t border-zinc-400 mt-2" />
                 </li>
-
                 <li>
                   <a
                     href="https://instagram.com"
@@ -82,18 +129,52 @@ const App: React.FC = () => {
                     <Instagram className="w-6 h-6" />
                     Instagram
                   </a>
-                  <hr className="w-72 border-t border-zinc-400 mt-2" />
+                  <hr className="w-full border-t border-zinc-400 mt-2" />
                 </li>
               </ul>
             </div>
 
-            <div className="md:w-1/2">
+            <div className="md:w-1/2 flex justify-center md:justify-end">
               <img
-                // src={FotoPessoal} // Substitua pela URL da sua foto pessoal
+                src={FotoPessoal}
                 alt="Rafael Augusto Klein"
-                className="w-96 rounded-lg shadow-xl object-cover"
+                className="w-60 md:w-96 rounded-lg shadow-xl object-cover"
               />
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-12 bg-black">
+        <div className="container mx-auto px-4">
+          <h3 className="text-3xl font-bold text-start mb-6 text-zinc-100">Certificações</h3>
+          <hr className="w-56 border-t border-zinc-400 mt-2 mb-10" />
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {certificates.map((cert, index) => (
+              <div
+                key={index}
+                className="bg-zinc-900 p-6 rounded-3xl shadow-lg hover:bg-zinc-800 hover:shadow-xl transform hover:scale-105 transition duration-300 group"
+              >
+                <h3 className="text-xl font-semibold">{cert.title}</h3>
+                <p className="text-zinc-400">{cert.issuer}</p>
+                <p className="text-sm text-zinc-400">{cert.date}</p>
+                {cert.skills && (
+                  <ul className="mt-2 text-sm text-zinc-400">
+                    {cert.skills.map((skill, i) => (
+                      <li key={i}>- {skill}</li>
+                    ))}
+                  </ul>
+                )}
+                <a
+                  href={cert.path}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mt-4 inline-block text-violet-500 hover:underline text-sm"
+                >
+                  Ver certificado
+                </a>
+              </div>
+            ))}
           </div>
         </div>
       </section>
@@ -140,7 +221,6 @@ const App: React.FC = () => {
         </div>
       </section>
 
-
       <section id="skills" className="bg-gradient-to-b from-black via-zinc-900 to-zinc-800 py-24">
         <div className="container mx-auto px-6">
           <h3 className="text-4xl font-bold text-center mb-6 text-zinc-100">Habilidades</h3>
@@ -174,7 +254,6 @@ const App: React.FC = () => {
         <div className="container mx-auto px-6 text-center">
           <h3 className="text-4xl font-bold text-zinc-100 mb-6">Projetos</h3>
           <hr className="w-72 border-t border-zinc-400 mx-auto mt-2 mb-8" />
-
           <p className="text-zinc-400 mb-12 max-w-2xl mx-auto text-lg">
             A seguir estão alguns projetos que desenvolvi ao longo da minha trajetória como desenvolvedor. Para explorar mais sobre eles ou sobre outros projetos basta clicar em algum que se interessar e observar toda a tecnologia e resultado por trás dele.
           </p>
@@ -238,55 +317,48 @@ const App: React.FC = () => {
             Veja mais no meu GitHub
             <ArrowRight className="ml-2" />
           </a>
-
         </div>
       </section>
 
-      <section id="contact" className="container mx-auto py-24 px-6">
-        <h3 className="text-3xl font-bold text-center mb-6 text-[#F2F2F0]">Contato</h3>
+      <section id="contact" className="container mx-auto py-40 px-6">
+        <h3 className="text-6xl font-bold text-center mb-6 text-zinc-100">Entre em contato</h3>
         <hr className="w-72 border-t border-zinc-400 mx-auto mt-2 mb-8" />
-
-        <form className="max-w-xl mx-auto bg-black p-6 rounded-lg shadow-lg">
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-zinc-300 mb-2">
-              Seu Nome
-            </label>
-            <input
-              type="text"
-              id="name"
-              className="w-full px-4 py-2 bg-zinc-900 border border-zinc-300 rounded-lg focus:outline-none focus:ring focus:ring-zinc-200"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-zinc-300 mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="w-full px-4 py-2 bg-zinc-900 border border-zinc-300 rounded-lg focus:outline-none focus:ring focus:ring-zinc-200"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="message" className="block text-zinc-300 mb-2">
-              Mensagem
-            </label>
-            <textarea
-              id="message"
-              rows={4}
-              className="w-full px-4 py-2 bg-zinc-900 border border-zinc-300 rounded-lg focus:outline-none focus:ring focus:ring-zinc-200"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full px-6 py-3 bg-zinc-500 hover:bg-zinc-700 text-white font-medium rounded-lg shadow-md transform hover:scale-105 transition duration-300"
-          >
-            Enviar
-          </button>
-        </form>
+        <div className="flex bg-zinc-900 justify-center space-x-4 rounded-full px-4 py-3 max-w-md mx-auto">
+          <Mail />
+          <h3 className='text-zinc-300'>rafael.augustoklein04@gmail.com</h3>
+        </div>
+        <div className="flex justify-center space-x-4 mt-8">
+          <ul className="flex space-x-6">
+            <li>
+              <a
+                href="https://www.linkedin.com"
+                className="flex items-center gap-2 font-semibold text-gray-400 hover:text-violet-500"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com"
+                className="flex items-center gap-2 font-semibold text-gray-400 hover:text-violet-500"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://instagram.com"
+                className="flex items-center gap-2 font-semibold text-gray-400 hover:text-violet-500"
+              >
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
       </section>
 
-      <footer className="bg-[#0D0D0D] py-6 text-center">
+
+      <footer className="bg-gradient-to-b from-black via-zinc-950 to-zinc-900 py-6 text-center">
         <p className="text-[#79818C] text-sm">&copy; 2024 Rafael. Todos os direitos reservados.</p>
       </footer>
     </div>
